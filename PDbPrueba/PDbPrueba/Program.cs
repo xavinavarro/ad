e82f33338ea17f62fc67bf7346df8dc0a46345d4
XaviNavarro <dam.xnavarro@institutoserpis.org>
2016-09-20 09:07:24 +0200

    Actualizando PDbprueba

--- PDbPrueba/PDbPrueba/Program.cs
+++ PDbPrueba/PDbPrueba/Program.cs
@@ -1,39 +1,64 @@
-using System;
 using MySql.Data.MySqlClient;
+using System;
+using System.Data;
 
-namespace PDbPrueba
-{
-	class MainClass
-	{
-		public static void Main (string[] args)
-		{
-			Console.WriteLine ("Probando acceso a dbprueba");
-			MySqlConnection mySqlConnection = new MySqlConnection ("Database=dbprueba;User Id=root;Password=sistemas");
 
-			mySqlConnection.Open ();
+namespace PDbPrueba {
+
+	class MainClass	{
+
+		public static void Main (string[] args){
+
+		//	Console.WriteLine ("Probando acceso a dbprueba");
+			IDbConnection dbConnection = new MySqlConnection (
+				"Database=dbprueba;User Id=root;Password=sistemas"
+			);
+
+			dbConnection.Open ();
 
 			//Operaciones...
-			//mySqlConnection.CreateCommand ().ExecuteScalar ();
+			IDbCommand dbCommand = dbConnection.CreateCommand ();
+			dbCommand.CommandText = "insert into categoria (nombre) values (@nombre)";
+			IDbDataParameter dbDataParameter = dbCommand.CreateParameter ();
+			dbDataParameter.ParameterName = "nombre";
+			dbDataParameter.Value = "Categoria 4";
+			dbCommand.Parameters.Add (dbDataParameter);
+			dbCommand.ExecuteNonQuery ();
 
+			
 
-			mySqlConnection.Close ();
+			dbConnection.Close ();
+
+			//readLong ("Introduce el id: ");
 		}
 
-		private void close (){
+		private static long readLong(string label) {
+			while (true) {
+				Console.Write (label);
+				string data = Console.ReadLine ();
+				try {
+					return long.Parse (data);
+				} catch {
+					Console.WriteLine ("Solo numeros, introduce un numero por favor: ");
+				}
+			}
+		}
+
+	/*	private void close (){
 
 		}
 
 		private void update (){
-		
+			
 		}
 
 		private void insert (){
-		
+			
 		}
 
 		private void delete (){
-		
-		}
+			
+		}*/
 
 
 
