efb7171603a1c6cbe03baba853d0692c30d6bd14
XaviNavarro <dam.xnavarro@institutoserpis.org>
2016-09-22 10:31:25 +0200

    PDbPrueba actualizado

--- PDbPrueba/PDbPrueba/Program.cs
+++ PDbPrueba/PDbPrueba/Program.cs
@@ -9,67 +9,67 @@ namespace PDbPrueba {
 
 		public static void Main (string[] args){
 
-		//	Console.WriteLine ("Probando acceso a dbprueba");
+			//	Console.WriteLine ("Probando acceso a dbprueba");
 			IDbConnection dbConnection = new MySqlConnection (
 				"Database=dbprueba;User Id=root;Password=sistemas"
-			);
+			);a
+
 
 			dbConnection.Open ();
 
-	/*		//Operaciones...
-			IDbCommand dbCommand = dbConnection.CreateCommand ();
-			dbCommand.CommandText = "insert into categoria (nombre) values (@nombre)";
-			IDbDataParameter dbDataParameter = dbCommand.CreateParameter ();
-			dbDataParameter.ParameterName = "nombre";
-			dbDataParameter.Value = "Categoria 4";
-			dbCommand.Parameters.Add (dbDataParameter);
-			dbCommand.ExecuteNonQuery ();
-*/
-			//Para select...
-			IDbCommand dbCommand = dbConnection.CreateCommand ();
-			dbCommand.CommandText = "select * from categoria";
-			IDataReader dataReader = dbCommand.ExecuteReader ();
-			while (dataReader.Read()) {
-				Console.WriteLine ("{0,4} {1}", dataReader ["id"], dataReader ["nombre"]);
+			getOpcion ();
+
+			Console.Clear ();
+
+
+				while (true){
+					Console.WriteLine ("*--MENU--*");
+					Console.WriteLine ("1.- Nuevo");
+					Console.WriteLine ("2.- Editar");
+					Console.WriteLine ("3.- Borrar");
+					Console.WriteLine ("4.- Listar");
+					Console.WriteLine ("5.- Salir");
+					Console.WriteLine ("Elige una opcion: ");
+
+					string opcion = Console.ReadLine ();
+					if (Regex.IsMatch (opcion, "^[12345]$"))
+						return int.Parse (opcion);
+
+					Console.WriteLine("Opcion no valida, introduce un numero valido.");
+
+				switch(opcion){
+
+
+					case "1":
+						Console.Clear();
+
+						return 1;
+
+					case "2":
+						Console.Clear();
+
+						return 2;
+
+					case "3":
+						Console.Clear();
+
+						return 3;
+
+					case "4":
+						Console.Clear();
+
+						return 4;
+
+					case "5":
+						Console.Clear();
+
+						return 5;
+
+				}
+
 			}
-			dataReader.Close ();
-
-
 			dbConnection.Close ();
 
-			//readLong ("Introduce el id: ");
 		}
-
-		private static long readLong(string label) {
-			while (true) {
-				Console.Write (label);
-				string data = Console.ReadLine ();
-				try {
-					return long.Parse (data);
-				} catch {
-					Console.WriteLine ("Solo numeros, introduce un numero por favor: ");
-				}
-			}
-		}
-
-	/*	private void close (){
-
-		}
-
-		private void update (){
-			
-		}
-
-		private void insert (){
-			
-		}
-
-		private void delete (){
-			
-		}*/
-
-
-
-
 	}
 }
