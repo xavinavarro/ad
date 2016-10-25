71555040f2d64f011570311a412627c2897436be
XaviNavarro <dam.xnavarro@institutoserpis.org>
2016-10-25 10:45:22 +0200

    Update

---
+++ Org.InstitutoSerpis.Ad/Org.InstitutoSerpis.Ad/Null.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace Org.InstitutoSerpis.Ad
+{
+	public class Null
+	{
+		public static readonly Null Value = new Null();
+	}
+}
+
---
+++ PArticulo/PArticulo/App.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Data;
+
+namespace PArticulo
+{
+	public class App {
+		private App(){
+		}
+
+		private static App instance = new App ();
+		public static App Instance {
+			get{ return instance;}
+		}
+
+		private IDbConnection dbConnection;
+		public IDbConnection DbConnection {
+			get{ return dbConnection;}
+			set{ dbConnection = value;}
+		}
+	}
+}
+
