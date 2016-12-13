df8cb1f19c2a34de62be0d69c36ab6a382b32e17
xavinavarro <dam.xnavarro@institutoserpis.org>
2016-12-13 10:08:05 +0100

    Actualizando ArticuloDao

--- GHolaMundo/src/main/java/org/institutoserpis/ad/ArticuloDao.java
+++ GHolaMundo/src/main/java/org/institutoserpis/ad/ArticuloDao.java
@@ -2,6 +2,7 @@ package org.institutoserpis.ad;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -11,8 +12,11 @@ public class ArticuloDao {
 	public static void main(String[] args) throws SQLException {
 		Connection connection = DriverManager.getConnection(
 			"jdbc:mysql://localhost/dbprueba", "root", "sistemas");
-		Statement statement = connection.createStatement();
-		ResultSet resultSet = statement.executeQuery("select * from articulo");
+		PreparedStatement preparedStatement = 
+				connection.prepareStatement("select * from articulo where id > ?");
+		preparedStatement.setObject(1, Long.parseLong(args[0]));
+		ResultSet resultSet = preparedStatement.executeQuery();
+		
 		System.out.printf("%5s %-30s %10s %9s\n", "id", "nombre", "precio", "categoria");
 		while (resultSet.next()) {
 			System.out.printf("%5s %-30s %10s %9s\n", 
@@ -22,7 +26,7 @@ public class ArticuloDao {
 					resultSet.getObject("categoria")
 			);
 		}
-		statement.close();
+		preparedStatement.close();
 		connection.close();
 		System.out.println("fin");
 	}
