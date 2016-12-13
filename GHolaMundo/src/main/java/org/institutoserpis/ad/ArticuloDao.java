a9371ce3fdd2ea6d69f8be44f7688d716f4ce97b
xavinavarro <dam.xnavarro@institutoserpis.org>
2016-12-13 09:26:25 +0100

    Añadido ArticuloDao

--- GHolaMundo/src/main/java/org/institutoserpis/ad/ArticuloDao.java
+++ GHolaMundo/src/main/java/org/institutoserpis/ad/ArticuloDao.java
@@ -1,8 +1,30 @@
 package org.institutoserpis.ad;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class ArticuloDao {
 
-	public static void main(String[] args){
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws SQLException {
+		Connection connection = DriverManager.getConnection(
+			"jdbc:mysql://localhost/dbprueba", "root", "sistemas");
+		Statement statement = connection.createStatement();
+		ResultSet resultSet = statement.executeQuery("select * from articulo");
+		System.out.printf("%5s %-30s %10s %9s\n", "id", "nombre", "precio", "categoria");
+		while (resultSet.next()) {
+			System.out.printf("%5s %-30s %10s %9s\n", 
+					resultSet.getObject("id"), 
+					resultSet.getObject("nombre"),
+					resultSet.getObject("precio"),
+					resultSet.getObject("categoria")
+			);
+		}
+		statement.close();
+		connection.close();
+		System.out.println("fin");
 	}
+
 }
